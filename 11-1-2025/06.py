56c9e74f80f9c63fef8a45e2da3b2531c7928ec6
Harsh-Dh <harshdh54@outlook.com>
2025-01-11 17:14:35 +0530

    update

---
+++ 11-1-2025/06.py
@@ -0,0 +1,12 @@
+a=1
+a1=1.1
+a2=complex(8+2)
+b="Harsh"
+c=True
+d=None
+print("this is type of a", type(a))
+print("this is type of a1", type(a1))
+print("this is type of a2", type(a2))
+print("this is type of b", type(b))
+print("this is type of c", type(c))
+print("this is type of d", type(d))
---
+++ 11-1-2025/07.py
@@ -0,0 +1,7 @@
+print(5+7)#addition
+print(5-7)#substraction
+print(5*7)#multiplication
+print(10/7)#division
+print(10//7)#floor division
+print(5%7)#modulus
+print(5**3)#expontential
