ab6edc8362bdc6fab2bc0bb44a6fdd35af70964b
Harsh-Dh <harshdh54@outlook.com>
2025-01-15 15:09:50 +0530

    Update 09.py

--- 15-1-2025/09.py
+++ 15-1-2025/09.py
@@ -3,9 +3,11 @@
 a="Harsh"
 b="Dhiman"
 print(a+b)
+print(type(a),type(b))
 a="1"
 b="3"
 print(a+b)
+print(type(a),type(b))
 print(int(a)+int(b))
 
 #Implicit TypeCasting
@@ -13,3 +15,4 @@ print(int(a)+int(b))
 a=20.8
 b=4
 print(a+b)
+print(type(a),type(b))
