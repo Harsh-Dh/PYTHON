d1e977fd8d26368a88ba3070a4ba0e1718184b51
Harsh-Dh <harshdh54@outlook.com>
2025-01-16 14:34:19 +0530

    Create 10.py

---
+++ 16-1-2025/10.py
@@ -0,0 +1,12 @@
+#input
+
+name=input("Enter your name: ")
+print("Hey",name ,"Welcome to CALCULATOR")
+
+a = input("Enter 1st Number: ")
+b = input("Enter 2nd Number: ")
+
+print("Sum of two Numbers is ",int(a)+int(b))
+print("Sub of two Numbers is ",int(a)-int(b))
+print("Mul of two Numbers is ",int(a)*int(b))
+print("Division of two Numbers is ",int(a)/int(b))
