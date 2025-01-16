9f101ce6e738d596eea7e8fc010bb977ebbf676a
Harsh-Dh <harshdh54@outlook.com>
2025-01-16 15:17:24 +0530

    Create 12.py

---
+++ 16-1-2025/12.py
@@ -0,0 +1,11 @@
+#Slicing
+a="Harsh"
+len1=len(a)
+print(a +" is a", len1, "letter word.")
+print(a[0:4]) 
+print(a[:4])
+print(a[1:4])
+print(a[:])
+print(a[:-4])#5 - 4
+print(a[-1:-4])#5-1=4 & 5-4=1 ,,4 to 1 print????
+print(a[-4:-1])#5-4=1 & 5-1=4 ,,1 to 4 print
